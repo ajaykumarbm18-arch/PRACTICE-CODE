6722999b6389179cd5d4c2015ac0cd1fad0f3839
ajaykumarbm18-arch <ajaykumarbm18@gmail.com>
2025-11-14 16:10:57 +0530

    New Files are Added

---
+++ JAVA PROGRAMS/Java2.java
@@ -0,0 +1,8 @@
+public class Java2{
+    public static void main(String[] args){
+        int myAge = 25;
+        String myName  = "Ajay";
+        System.out.println("My name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("Hello World");
+    }
+}
---
+++ RUST PROGRAMS/rust2.rs
@@ -0,0 +1,5 @@
+fn main () {
+    let my_age: u8 = 25;
+    let my_name: &str = "Ajay";
+    println!("My name is {} and I am {} years old. ", my_name, my_age);
+}
